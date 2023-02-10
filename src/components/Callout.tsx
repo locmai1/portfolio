2450e494cf34d473960b6761f776ee6b2eee7fbd
Loc Mai <locmai0315@gmail.com>
2023-02-10 09:25:36 -0500

    add size to emoji component

--- src/components/Callout.tsx
+++ src/components/Callout.tsx
@@ -14,7 +14,7 @@ const Callout = ({
 }: CalloutProps) => {
   return (
     <div className='flex flex-row w-full bg-[#f1f1ef] whitespace-pre-wrap p-4 rounded m-5'>
-      <Emoji symbol={emoji} label={label}/>
+      <Emoji symbol={emoji} label={label} size='2xl'/>
       <div className='h-full w-full leading-8 text-lg ml-3'>
         <span className='font-bold'>Current Status: </span>
         <span className=''>{status}</span>
--- src/components/Emoji.tsx
+++ src/components/Emoji.tsx
@@ -3,14 +3,17 @@ import React from "react";
 type EmojiProps = {
   symbol: string;
   label: string;
+  size: string;
 }
 
 const Emoji = ({
   symbol,
-  label
+  label,
+  size
 }: EmojiProps) => {
   return (
-    <span className='text-2xl'
+    // <span className='text-2xl'
+    <span className={`text-${size}`}
       aria-label={label ? label : ""}
       aria-hidden={label ? "false" : "true"}
     >
