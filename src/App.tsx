1b754bda630fc5b74db6772c41dc345afa4d3f40
Loc Mai <locmai0315@gmail.com>
2023-02-10 06:41:05 -0500

    minor component change

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Callout from './components/Callout';
-import Emoji from './components/Emoji';
 // import logo from './logo.svg';
 // import './App.css';
 
 function App() {
   return (
     <div className="App font-poppins max-w-[900px] h-full mx-auto">
-      <Callout emoji={<Emoji symbol='⚠️' label='alert'/>} status={"seeking front-end & software engineering internships for summer 2023!"}/>
+      <Callout status={"seeking front-end & software engineering internships for summer 2023!"}
+        emoji='⚠️'
+        label='alert'
+      />
     </div>
   );
 }
--- src/components/Callout.tsx
+++ src/components/Callout.tsx
@@ -2,17 +2,19 @@ import React from 'react';
 import Emoji from './Emoji';
 
 type CalloutProps = {
-  emoji: JSX.Element;
+  emoji: string;
+  label: string;
   status: string;
 }
 
 const Callout = ({
   emoji,
+  label,
   status
 }: CalloutProps) => {
   return (
     <div className='flex flex-row w-full bg-[#f1f1ef] whitespace-pre-wrap p-4 rounded m-5'>
-      {emoji}
+      <Emoji symbol={emoji} label={label}/>
       <div className='h-full w-full leading-8 text-lg ml-3'>
         <span className='font-bold'>Current Status: </span>
         <span className=''>{status}</span>
